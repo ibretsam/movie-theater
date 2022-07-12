2b23b5d9acd9aef07c2d7d285f96ab86377b0c42
Khanh Le <khanhle.3109@gmail.com>
2022-07-12 13:08:42 +0700

    added react files

---
+++ front-end/movietheater/build/precache-manifest.0678358796cbb7625dc16a399ea12de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ee621b7eb3c8a3f612",
+    "url": "/static/css/main.4eb7b459.chunk.css"
+  },
+  {
+    "revision": "e2ee621b7eb3c8a3f612",
+    "url": "/static/js/main.233654de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45d7300df25fcc83907c",
+    "url": "/static/js/2.16edb8e7.chunk.js"
+  },
+  {
+    "revision": "d428723e242082ef0ee07e3a1aa5d26c",
+    "url": "/index.html"
+  }
+];
